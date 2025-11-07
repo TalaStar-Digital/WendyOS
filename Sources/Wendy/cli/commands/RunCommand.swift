ff56d77390f9f7276ba61c40e70a1a02a3418749
Joannis Orlandos <joannis@orlandos.nl>
2025-11-07 15:07:59 +0100

    Fix flaky Swift app uploads
    Sometimes if you were unlucky the swift app would be cleaned up before the upload was done. This would break the upload

--- Sources/Wendy/cli/commands/RunCommand.swift
+++ Sources/Wendy/cli/commands/RunCommand.swift
@@ -641,6 +641,13 @@ extension RunCommand {
             }
         }
 
+        let tempDir = FileManager.default.temporaryDirectory.appendingPathComponent(
+            UUID().uuidString
+        )
+        try FileManager.default.createDirectory(at: tempDir, withIntermediateDirectories: true)
+        defer {
+            try? FileManager.default.removeItem(at: tempDir)
+        }
         let (imageName, container) = try await Noora().progressStep(
             message: "Building container",
             successMessage: "Container built successfully!",
@@ -719,13 +726,6 @@ extension RunCommand {
             }
 
             progress("Building final container image")
-            let tempDir = FileManager.default.temporaryDirectory.appendingPathComponent(
-                UUID().uuidString
-            )
-            try FileManager.default.createDirectory(at: tempDir, withIntermediateDirectories: true)
-            defer {
-                try? FileManager.default.removeItem(at: tempDir)
-            }
             let container = try await buildDockerContainer(
                 image: imageSpec,
                 imageName: imageName,
