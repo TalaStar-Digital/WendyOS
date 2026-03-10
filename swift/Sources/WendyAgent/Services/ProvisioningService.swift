0b1805398bad9401cd73970448e966115b5cce20
Konstantin Bender <me@konstantinbender.com>
2026-03-10 17:36:42 +0100

    Stub out provisioning service responses instead of fatalError

--- swift/Sources/WendyAgent/Services/ProvisioningService.swift
+++ swift/Sources/WendyAgent/Services/ProvisioningService.swift
@@ -6,13 +6,15 @@ struct ProvisioningService: Wendy_Agent_Services_V1_WendyProvisioningService.Sim
         request: Wendy_Agent_Services_V1_StartProvisioningRequest,
         context: ServerContext
     ) async throws -> Wendy_Agent_Services_V1_StartProvisioningResponse {
-        fatalError("not implemented")
+        Wendy_Agent_Services_V1_StartProvisioningResponse()
     }
 
     func isProvisioned(
         request: Wendy_Agent_Services_V1_IsProvisionedRequest,
         context: ServerContext
     ) async throws -> Wendy_Agent_Services_V1_IsProvisionedResponse {
-        fatalError("not implemented")
+        var response = Wendy_Agent_Services_V1_IsProvisionedResponse()
+        response.notProvisioned = Wendy_Agent_Services_V1_NotProvisionedResponse()
+        return response
     }
 }
